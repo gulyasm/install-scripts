b00a331093cedc2db5a936847d6cd721057e7350
gulyasm <mgulyas86@gmail.com>
2015-01-23 11:27:24 +0100

    Added vim plugins install.

--- vim.sh
+++ vim.sh
@@ -7,3 +7,15 @@ make
 sudo make install
 cd ~
 rm -rf vim74
+
+# Install pathogen
+mkdir -p ~/.vim/autoload ~/.vim/bundle 
+curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+
+# Install ctrlp
+cd ~/.vim
+hg clone https://bitbucket.org/kien/ctrlp.vim bundle/ctrlp.vim
+
+# Install vim-go
+ cd ~/.vim/bundle
+ git clone https://github.com/fatih/vim-go.git
